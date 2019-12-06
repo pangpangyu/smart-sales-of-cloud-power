2b5bebd464e8dc141a285279ed03ff5b0d9e42ce
pangpangyu <1047510535@qq.com>
2019-12-06 21:05:14 +0800

    NewList

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Carousel } from 'antd-mobile';
-import api from '../api/index';
-import { baseUrl } from '../config/index';
+// import api from '../api/index';
+// import { baseUrl } from '../config/index';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component{
   constructor(props){
@@ -31,27 +32,49 @@ class Home extends React.Component{
 	}
   render(){
     return(
-      <div className="banner">
-        <Carousel
-          autoplay={false}
-          infinite
-          cellSpacing={8}
-          slideWidth={0.8}
-          dots={false}
-          afterChange={index => this.setState({ slideIndex: index })}
-          style={{padding:'20px 0',overflow:'inherit'}}
-        >
-          {this.state.carouselList.map((item,index) => (
-            <div 
-              className="banner-item"
-              key={index} >
-                <div className="banner-img" style={{transform: this.state.slideIndex === index ? 'scale(1)' : 'scale(0.9)',}}>
-                  <img src={ item.url } alt={item.name} style={{width:"100%"}}/>
-                </div>
-              {/* baseUrl +  */}
-            </div>
-          ))}
-        </Carousel>
+      <div>
+        <div className="banner">
+          <Carousel
+            autoplay={false}
+            infinite
+            cellSpacing={8}
+            slideWidth={0.8}
+            dots={false}
+            afterChange={index => this.setState({ slideIndex: index })}
+          >
+            {this.state.carouselList.map((item,index) => (
+              <div 
+                className="banner-item"
+                key={index} >
+                  <div className="banner-img" style={{transform: this.state.slideIndex === index ? 'scale(1)' : 'scale(0.9)',}}>
+                    <img src={ item.url } alt={item.name} style={{width:"100%"}}/>
+                  </div>
+                {/* baseUrl +  */}
+              </div>
+            ))}
+          </Carousel>
+        </div>
+        <div style={{height:'10px',background:'#f0f1f3'}}></div>
+        <div className="new-abstract">
+          <i className="iconfont iconxiaoxi"></i>
+          <div className="new-abstract">
+          <Carousel className="my-carousel"
+            vertical
+            dots={false}
+            dragging={false}
+            swiping={false}
+            autoplay
+            infinite
+            autoplayInterval={3000}
+            resetAutoplay={false}
+          >
+            {['ringringringringringringringringringringringringringringringringringringringring', 'ruby', 'iPhone', 'iPod', 'sorry', 'tourism', 'coke', 'ticket', 'note'].map(type => (
+              <div className="v-item" key={type}><Link to="/newList">{type}</Link></div>
+            ))}
+          </Carousel>
+          </div>
+        </div>
+        <div style={{height:'10px',background:'#f0f1f3'}}></div>
       </div>
     )
   }
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import App from '../App.js';
 import Index from '../view/index';
+import NewList from '../view/newList';
 import ErrorPage from '../view/ErrorPage';
 
 export default class ROUTER extends React.Component{
@@ -13,6 +14,7 @@ export default class ROUTER extends React.Component{
           <App>
             <Switch>
               <Route exact path="/" component={Index}></Route>
+              <Route path="/newList" component={NewList}></Route>
               <Route component={ErrorPage}></Route>
             </Switch>
           </App>
