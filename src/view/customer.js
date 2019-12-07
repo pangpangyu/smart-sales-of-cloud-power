0e18716e487fa60e7efdeb22cf65d3169fa53f93
pangpangyu <1047510535@qq.com>
2019-12-07 09:52:52 +0800

    jl

--- src/view/customer.js
+++ src/view/customer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from '../components/header'
+import Header from '../components/header';
+import { Link } from 'react-router-dom';
 /**
  * 客户管理
  */
@@ -9,7 +10,37 @@ export default class customer extends React.Component{
     return (
       <div style={{minHeight:'100vh',background:'#f0f1f3'}}>
         <Header title={'客户管理'} back={true} search={false}/>
-        123
+        <div className="customer-item">
+          <Link to="/">
+            <div className="icon icon1"></div>
+            <div className="title">电力用户信息</div>
+            <i className="iconfont iconyou"></i>
+          </Link>
+        </div>
+        <div style={{height:'10px'}}></div>
+        <div className="customer-item">
+          <Link to="/">
+            <div className="icon"><i className="iconfont iconziyuan"></i></div>
+            <div className="title">发电厂信息</div>
+            <i className="iconfont iconyou"></i>
+          </Link>
+        </div>
+        <div style={{height:'10px'}}></div>
+        <div className="customer-item">
+          <Link to="/">
+            <div className="icon"><i className="iconfont iconziyuan1"></i></div>
+            <div className="title">合作方信息</div>
+            <i className="iconfont iconyou"></i>
+          </Link>
+        </div>
+        <div style={{height:'10px'}}></div>
+        <div className="customer-item">
+          <Link to="/">
+            <div className="icon"><i className="iconfont iconshoudiangongsi"></i></div>
+            <div className="title">售电公司信息</div>
+            <i className="iconfont iconyou"></i>
+          </Link>
+        </div>
       </div>
     )
   }
