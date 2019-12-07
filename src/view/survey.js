34742b857262c61e9a342bf90e28417adf8c61f9
githubgym <2952378609@qq.com>
2019-12-07 20:37:19 +0800

    .

--- src/view/survey.js
+++ src/view/survey.js
@@ -1,43 +1,33 @@
-import React, {Fragment}from 'react';
+import React, { Fragment } from 'react';
 import Header from '../components/header';
+
 
 /**
  * 售电概况
  */
 
-export default class Survey extends React.Component{
+class Survey extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      data :[
-        { genre: 'Sports', sold: 275 },
-        { genre: 'Strategy', sold: 115 },
-        { genre: 'Action', sold: 120 },
-        { genre: 'Shooter', sold: 350 },
-        { genre: 'Other', sold: 150 }
-      ]
+     
     }
   }
 
-  componentDidMount(){
-  
-  }
-
-  render(){
-    return(
+  render() {
+    return (
       <Fragment>
-        <Header title={'售电概况'} back={true} search={false}/>
-        <div id="c1" className="charts">
-	      </div>
-     </Fragment>
+        <Header title={'售电概况'} back={true} search={false} />
+      </Fragment>
     )
   }
- 
+
 }
 
+export default Survey;
 
 
 
 
 
- 
+
