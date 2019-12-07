379bf97acb33befb2df862131f17c544bf1894b3
Wei-lgtm <51849050+Wei-lgtm@users.noreply.github.com>
2019-12-07 10:04:07 +0800

    z.1207.1

--- src/view/todoDetLc.js
+++ src/view/todoDetLc.js
@@ -18,16 +18,7 @@ export default class Todolist extends React.Component {
                         <p>下图为示例图，具体流程图按照查看的流程图显示</p>
                     </div>
                     <div className="cont">
-                        <div className="step_list">
-                            <ul>
-                                <li>开始</li>
-                                <li><span>1</span>步骤1</li>
-                                <li><span>2</span>步骤2</li>
-                                <li><span>3</span>步骤3</li>
-                                <li><span>4</span>步骤4</li>
-                                <li>结束</li>
-                            </ul>
-                        </div>
+                        
                     </div>
                     <div className="f_btn">
                         <a href="#">关闭</a>
