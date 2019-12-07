b0386ea47e98c830d9ac4639072845ef8a2453ee
pangpangyu <1047510535@qq.com>
2019-12-07 18:44:32 +0800

    jl

--- src/view/survey.js
+++ src/view/survey.js
@@ -1,4 +1,4 @@
-import React, {Fragment}from 'react';
+import React from 'react';
 import Header from '../components/header';
 
 /**
@@ -15,6 +15,13 @@ export default class Survey extends React.Component{
         { genre: 'Action', sold: 120 },
         { genre: 'Shooter', sold: 350 },
         { genre: 'Other', sold: 150 }
+      ],
+      total:[
+        { id:1,num:'2000',title:'合同总数' },
+        { id:2,num:'2000',title:'合同总数' },
+        { id:3,num:'2000',title:'合同总数' },
+        { id:4,num:'2000',title:'合同总数' },
+        { id:5,num:'2000',title:'合同总数' }
       ]
     }
   }
@@ -25,11 +32,19 @@ export default class Survey extends React.Component{
 
   render(){
     return(
-      <Fragment>
+      <div className="survey-page" style={{minHeight:'100vh',background:'#f0f1f3'}}>
         <Header title={'售电概况'} back={true} search={false}/>
+        <div className="top-totls">
+          { this.state.total && this.state.total.map(item => {
+            return  <div className="item" key={item.id}>
+                      <p><span>{ item.num }</span>个</p>
+                      <p>{ item.title }</p>
+                    </div>
+          }) }
+        </div>
         <div id="c1" className="charts">
 	      </div>
-     </Fragment>
+     </div>
     )
   }
  
