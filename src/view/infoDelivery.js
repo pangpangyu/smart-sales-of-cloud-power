3f18a9d0eb9c23542ba477cc38c3e21dc674060f
pangpangyu <1047510535@qq.com>
2019-12-09 20:04:01 +0800

    jl

--- src/view/infoDelivery.js
+++ src/view/infoDelivery.js
@@ -5,9 +5,22 @@ import React from 'react';
  * */
 
 export default class InfoDelivery extends React.Component{
+
+  gotoback = () => {
+    window.history.go(-1)
+  }
+
   render(){
     return(
-      <div className="infoDelivey" style={{minHeight:'100vh',background:'#f0f1f3'}}>123</div>
+      <div className="infoDelivey" style={{minHeight:'100vh',background:'#f0f1f3'}}>
+        <div className="header">
+          <p className="header-title">信息发布</p>
+          <div className="header-back" onClick={this.gotoback}><i className="iconfont iconfanhui"></i></div>
+          <div className="header-search"><i className="iconfont iconsousuo"></i></div>
+        </div>
+        <div style={{height:'45px'}}></div>
+        123
+      </div>
     )
   }
 }
