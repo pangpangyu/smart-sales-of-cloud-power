27853029926dcefa2cea1196b970e294d66d5cbe
pangpangyu <1047510535@qq.com>
2019-12-09 22:07:22 +0800

    jl

--- src/components/home.js
+++ src/components/home.js
@@ -22,7 +22,7 @@ class Home extends React.Component{
         { id:3 ,name:'合同管理',url:require('../assets/img/img003.png'),link:'/contractManage' },
         { id:4 ,name:'结算管理',url:require('../assets/img/img004.png'),link:'/' },
         { id:5 ,name:'电量跟踪',url:require('../assets/img/img005.png'),link:'/' },
-        { id:6 ,name:'经营分析',url:require('../assets/img/img006.png'),link:'/' },
+        { id:6 ,name:'经营分析',url:require('../assets/img/img006.png'),link:'/bsinessAnalysis' },
         { id:7 ,name:'考勤管理',url:require('../assets/img/img007.png'),link:'/' },
         { id:8 ,name:'信息发布',url:require('../assets/img/img008.png'),link:'/infoDelivey' },
         { id:9 ,name:'交易中心',url:require('../assets/img/img009.png'),link:'/' },
---
+++ src/view/businessAnalysis.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/**
+ * 经营分析
+ */
+
+export default class BsinessAnalysis extends React.Component{
+  render(){
+    return(
+      <div>123</div>
+    )
+  }
+}
