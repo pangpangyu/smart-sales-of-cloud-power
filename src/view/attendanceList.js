d202b9c77c2204070dda1c4c4c9986a324ca0566
githubgym <2952378609@qq.com>
2019-12-10 00:25:50 +0800

    radioGroup

--- src/view/attendanceList.js
+++ src/view/attendanceList.js
@@ -72,27 +72,30 @@ class AttendanceList extends React.Component {
                     
                 </ul>
 
-                <div className="mask" style={{display:'none'}} >
+                <div className="mask"  >
                     <div className="attedance-dialog">
                         <div className="tit">考勤管理</div>
                         <div className="cont">
                             <div className="item">
                                 <img className="img" src={require('../assets/img/img105.png')} alt=""/>
                                 <div class="self-radio">
-                                    <label htmlFor="r1">
-                                        <input  id="r1" type="radio" name="attedance" />
-                                        <img className="label-icon" src={require('../assets/img/img108.png')} alt=""/>
-                                        <img className="label-icon checked" src={require('../assets/img/img109.png')} alt=""/>
-                                    </label>
+                                    <input  id="r1" type="radio" value={"qjsq"}  name="attedance" onChange={this.handleCheckChanged} />
+                                    <label htmlFor="r1">请假申请</label>
                                 </div>
                             </div>
                             <div className="item">
                                 <img className="img" src={require('../assets/img/img106.png')} alt=""/>
-                                <div><input type="radio" name="attedance" /></div>
+                                <div class="self-radio">
+                                    <input  id="r2" type="radio" value={"wcsq"} name="attedance" onChange={this.handleCheckChanged} />
+                                    <label htmlFor="r2">外出申请</label>
+                                </div>
                             </div>
                             <div className="item">
                                 <img className="img" src={require('../assets/img/img107.png')} alt=""/>
-                                <div><input type="radio" name="attedance" /></div>
+                                <div class="self-radio">
+                                    <input  id="r3" type="radio" value={"jbsq"} name="attedance" onChange={this.handleCheckChanged} />
+                                    <label htmlFor="r3">加班申请</label>
+                                </div>
                             </div>
                         </div>
                         <div className="btn-group">
@@ -116,6 +119,10 @@ class AttendanceList extends React.Component {
         this.setState({
             attendanceList:[]
         })
+    }
+
+    handleCheckChanged=e=>{
+        console.log(e.target.value)
     }
 }
 
